ce4ece87c441706c1834d7f9b6e767fd41ffe12a
eipi10 <eipi10@qq.com>
2015-11-24 14:39:09 +0800

    add user/password as function parameters

--- cluster-create-user.sh
+++ cluster-create-user.sh
@@ -5,10 +5,12 @@ source $scriptpath/config.sh
 
 orignaluser=$1
 orignalpassword=$2
+user1=${3:-$user}
+password1=${4:-$password}
 
 for server in "${servers_cwp[@]}"
 do
   echo $server 
-  ~/cluster/cluster-install/create-user.sh $server $orignaluser $orignalpassword $user $password
+  ~/cluster/cluster-install/create-user.sh $server $orignaluser $orignalpassword $user1 $password1
 done
 
