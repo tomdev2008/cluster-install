6d8156a0107a0cf38f54e7029751edc9c7f120fd
eipi10 <eipi10@qq.com>
2015-11-27 15:08:26 +0800

    copy config.sh 1

--- manual-without-password.sh
+++ manual-without-password.sh
@@ -7,7 +7,7 @@ source $scriptpath/config.sh
 for server in "${servers_cwp[@]}"
 do
   echo $user@$server 
-  ssh $user@$server ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+  ssh $user@$server "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa"
   ssh-copy-id $user@$server
   scp $user@$server:~/.ssh/id_rsa.pub $scriptpath/id_rsa.pub.$server  
   cat $scriptpath/id_rsa.pub.$server >>~/.ssh/authorized_keys
