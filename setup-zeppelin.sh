d7e38b602f628c0fc99c535adaba1335be22577e
eipi10 <eipi10@qq.com>
2015-12-03 07:21:02 +0800

    zeppelin update

--- setup-zeppelin.sh
+++ setup-zeppelin.sh
@@ -13,6 +13,13 @@ fi
 rm -rf $aapath/zeppelin-incubator
 ln -s $aapath/zeppelin-0.5.5-incubating $aapath/zeppelin-incubator
 cd $aapath/zeppelin-incubator
-mvn clean package -DskipTests -Pspark-1.5 -Phadoop-2.6 -Ppyspark
 
+#sudo yum install -y npm 
+#sudo yum install -y libfontconfig 
+mvn clean package -DskipTests -Pspark-1.5 -Phadoop-2.6 
+cp $aapath/zeppelin-incubator/conf/zeppelin-env.sh.template $aapath/zeppelin-incubator/conf/zeppelin-env.sh
+export MASTER=spark://$spark_master:7077
+export SPARK_HOME=$spark_home
+export HADOOP_HOME=$hadoop_home
 
+cp $aapath/zeppelin-incubator/conf/zeppelin-site.xml.template $aapath/zeppelin-incubator/conf/zeppelin-site.xml
