8bbaf4db296b95fe779421d6fcf4208a3001acb4
eipi10 <eipi10@qq.com>
2015-12-03 15:45:17 +0800

    add temp command

--- for-command.sh
+++ for-command.sh
@@ -12,8 +12,9 @@ declare -a servers_cwp1=\(\"${servers1//,/\" \"}\"\)
 rm -rf $slaves
 for server in "${servers_cwp1[@]}"
 do
-echo ssh $user@$server eval $command 
-ssh $user@$server eval $command 
+command1=\"$command\"
+echo ssh $user@$server eval $command1 
+ssh $user@$server eval $command1
 #ssh $user@$server chmod 755 $scriptpath/temp.sh
 #ssh $user@$server $scriptpath/temp.sh
 #ssh $user@$server rm -rf $scriptpath/temp.sh
