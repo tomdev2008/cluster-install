2ad4673f8c195749aa78a07e3ea04c83e5a285cb
eipi10 <eipi10@qq.com>
2015-12-03 14:48:31 +0800

    change format

--- for-command.sh
+++ for-command.sh
@@ -12,8 +12,8 @@ declare -a servers_cwp1=\(\"${servers1//,/\" \"}\"\)
 rm -rf $slaves
 for server in "${servers_cwp1[@]}"
 do
-command1=\"$command\"
-echo ssh $user@$server $command1
-ssh $user@$server $command1
+#command1=\"$command\"
+echo ssh $user@$server $command
+ssh $user@$server $command
 done
 
