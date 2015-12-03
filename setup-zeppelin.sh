f7dc4733fbfce1d8295506aed138d117e2ca7484
eipi10 <eipi10@qq.com>
2015-12-03 09:02:03 +0800

    get source from git

--- setup-zeppelin.sh
+++ setup-zeppelin.sh
@@ -4,16 +4,17 @@ scriptpath=$(dirname "$script")
 source $scriptpath/config.sh
 
 oldpath=`pwd`
+cd $aapath
 
-if [ ! -d $aapath/zeppelin-0.5.5-incubating  ]
-then
-  wget http://mirror.olnevhost.net/pub/apache/incubator/zeppelin/0.5.5-incubating/zeppelin-0.5.5-incubating.tgz
-  tar -zxf zeppelin-0.5.5-incubating.tgz -C $aapath
-  rm -rf zeppelin-0.5.5-incubating.tgz
-fi
+#if [ ! -d $aapath/zeppelin-0.5.5-incubating  ]
+#then
+#  wget http://mirror.olnevhost.net/pub/apache/incubator/zeppelin/0.5.5-incubating/zeppelin-0.5.5-incubating.tgz
+#  tar -zxf zeppelin-0.5.5-incubating.tgz -C $aapath
+#  rm -rf zeppelin-0.5.5-incubating.tgz
+#fi
 
-rm -rf $aapath/incubator-zeppelin
-ln -s $aapath/zeppelin-0.5.5-incubating $aapath/incubator-zeppelin
+#rm -rf $aapath/incubator-zeppelin
+#ln -s $aapath/zeppelin-0.5.5-incubating $aapath/incubator-zeppelin
 
 
 #if [ ! -d $aapath/incubator-zeppelin  ]
@@ -23,9 +24,18 @@ ln -s $aapath/zeppelin-0.5.5-incubating $aapath/incubator-zeppelin
 #  git clone https://github.com/apache/incubator-zeppelin.git
 #fi
 
+#cd $aapath/incubator-zeppelin
+#mvn clean package -DskipTests -Pspark-1.5 -Phadoop-2.6 
 
+if [ ! -d $aapath/incubator-zeppelin  ]
+then 
+  wget http://www.carfab.com/apachesoftware/incubator/zeppelin/0.5.5-incubating/zeppelin-0.5.5-incubating-bin-all.tgz
+  tar -zxf zeppelin-0.5.5-incubating-bin-all.tgz -C $aapath
+  mv  zeppelin-0.5.5-incubating-bin-all incubator-zeppelin
+  rm -rf zeppelin-0.5.5-incubating-bin-all.tgz
+fi   
 cd $aapath/incubator-zeppelin
-mvn clean package -DskipTests -Pspark-1.5 -Phadoop-2.6 
+
 cp conf/zeppelin-env.sh.template conf/zeppelin-env.sh
 echo export MASTER=spark://$spark_master:7077                                 >> conf/zeppelin-env.sh
 echo export SPARK_HOME=$spark_home                                            >> conf/zeppelin-env.sh
