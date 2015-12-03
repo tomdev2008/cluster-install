740b8fd99f621173422770bda7686108d7b8329f
eipi10 <eipi10@qq.com>
2015-12-03 08:15:51 +0800

    get source from git

--- setup-zeppelin.sh
+++ setup-zeppelin.sh
@@ -3,23 +3,32 @@ script=$(readlink -f "$0")
 scriptpath=$(dirname "$script")
 source $scriptpath/config.sh
 
-if [ ! -d $aapath/zeppelin-0.5.5-incubating  ]
-then
-  wget http://mirror.olnevhost.net/pub/apache/incubator/zeppelin/0.5.5-incubating/zeppelin-0.5.5-incubating.tgz
-  tar -zxf zeppelin-0.5.5-incubating.tgz -C $aapath
-  rm -rf zeppelin-0.5.5-incubating.tgz
-fi
+#if [ ! -d $aapath/zeppelin-0.5.5-incubating  ]
+#then
+#  wget http://mirror.olnevhost.net/pub/apache/incubator/zeppelin/0.5.5-incubating/zeppelin-0.5.5-incubating.tgz
+#  tar -zxf zeppelin-0.5.5-incubating.tgz -C $aapath
+#  rm -rf zeppelin-0.5.5-incubating.tgz
+#fi
 
-rm -rf $aapath/zeppelin-incubator
-ln -s $aapath/zeppelin-0.5.5-incubating $aapath/zeppelin-incubator
-cd $aapath/zeppelin-incubator
+#rm -rf $aapath/zeppelin-incubator
+#ln -s $aapath/zeppelin-0.5.5-incubating $aapath/zeppelin-incubator
+#cd $aapath/zeppelin-incubator
 
-#sudo yum install -y npm 
-#sudo yum install -y libfontconfig 
+oldpath=`pwd`
+cd $aapath
+git clone https://github.com/apache/incubator-zeppelin.git
+cd incubator-zeppelin
+
 mvn clean package -DskipTests -Pspark-1.5 -Phadoop-2.6 
 cp $aapath/zeppelin-incubator/conf/zeppelin-env.sh.template $aapath/zeppelin-incubator/conf/zeppelin-env.sh
 export MASTER=spark://$spark_master:7077
 export SPARK_HOME=$spark_home
-export HADOOP_HOME=$hadoop_home
+export SPARK_SUBMIT_OPTIONS="--driver-memory 512M --executor-memory 1G"
+export HADOOP_CONF_DIR==$hadoop_home/etc/hadoop
+export ZEPPELIN_PORT=9090
+#export ZEPPELIN_JAVA_OPTS=""
 
 cp $aapath/zeppelin-incubator/conf/zeppelin-site.xml.template $aapath/zeppelin-incubator/conf/zeppelin-site.xml
+
+cd $oldpath
+
