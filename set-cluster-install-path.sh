7c2fac255240683f466b34b896f7446d34de754f
eipi10 <eipi10@qq.com>
2016-02-19 17:38:19 +0800

    add set-cluster-install-path

--- set-cluster-install-path.sh
+++ set-cluster-install-path.sh
@@ -5,7 +5,7 @@ source $scriptpath/config.`hostname`
 
 echo 'set /etc/profile.d/cluster-install.sh'
 echo export PATH='$scriptpath:$PATH' > $targetpath/cluster-install.sh
-sudo mv $targetpath/scala.sh /etc/profile.d
+sudo mv $targetpath/cluster-install.sh /etc/profile.d
 
 $scriptpath/command-with-text.sh "cat /etc/profile.d/cluster-install.sh"
 
