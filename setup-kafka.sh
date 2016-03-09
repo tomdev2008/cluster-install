9d856a9e516d5edb4c8883409d100cfdc272345b
eipi10 <eipi10@qq.com>
2016-03-09 16:33:00 +0800

    d

--- setup-kafka.sh
+++ setup-kafka.sh
@@ -39,6 +39,7 @@ fi
 let i++                   
 done
 
+echo $kafka_log_dir
 sed -i "s/{broker.id}/$broker_id/g"      $kafka_home/config/server.properties
 sed -i "s/{log.dirs}/$kafka_log_dir/g"   $kafka_home/config/server.properties
 sed -i "s/{zookeeper.connect}/$zookeeper_connect/g" $kafka_home/config/server.properties
