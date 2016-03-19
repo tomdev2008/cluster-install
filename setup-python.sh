0e7a3e7ac1dc7f98e2e2f092e8c59efdf7da3492
eipi10 <eipi10@qq.com>
2016-03-19 17:42:51 +0800

    d

--- setup-python.sh
+++ setup-python.sh
@@ -3,14 +3,16 @@ script=$(readlink -f "$0")
 scriptpath=$(dirname "$script")
 source $scriptpath/config.`hostname`
 
-$scriptpath/software-untar.sh $python_home $python_folder $python_jar
 
-echo 'set /etc/profile.d/python.sh'
-echo export python_HOME=$python_home  > $targetpath/python.sh
-echo export PATH='$python_HOME/bin:$PATH' >> $targetpath/python.sh
-sudo mv $targetpath/python.sh /etc/profile.d
-
-$scriptpath/command-with-text.sh "ls -l $python_home"
-$scriptpath/command-with-text.sh "ls -l $aapath | grep $python_folder"
-$scriptpath/command-with-text.sh "cat /etc/profile.d/python.sh"
+if [ ! -e /usr/local/bin/python ]
+then
+  cd $targetpath/software
+  ll
+  sudo rm -rf $targetpath/software/$python_folder
+  tar zxvf $python_jar
+  cd $python_folder
+  ./configure
+  make
+  sudo make install
+fi
 
