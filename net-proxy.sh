fb600cb98e0108a000d97b202f8fb7dffb3fc3d3
eipi10 <eipi10@qq.com>
2016-03-19 18:13:18 +0800

    d

--- net-proxy.sh
+++ net-proxy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #set net proxy 
 #sudo -s
-if ! grep -q -i '^proxy=http://web-proxy.atl.hp.com:8080$' /etc/yum.conf; then echo proxy=http://web-proxy.atl.hp.com:8080 >> /etc/yum.conf; fi
-if ! grep -q -i '^http_proxy=http://web-proxy.atl.hp.com:8080$' /etc/profile ; then echo http_proxy=http://web-proxy.atl.hp.com:8080 >> /etc/profile ; fi
-if ! grep -q -i '^https_proxy=http://web-proxy.atl.hp.com:8080$' /etc/profile ; then echo https_proxy=http://web-proxy.atl.hp.com:8080 >> /etc/profile ; fi
-if ! grep -q -i '^http_proxy=http://web-proxy.atl.hp.com:8080$' /etc/wgetrc ; then echo http_proxy=http://web-proxy.atl.hp.com:8080 >> /etc/wgetrc ; fi
+if [ ! "$http_proxy" = "" ]
+then 
+  if ! grep -q -i "^proxy=$http_proxy$" /etc/yum.conf; then echo proxy=$http_proxy >> /etc/yum.conf; fi
+  if ! grep -q -i "^http_proxy=$http_proxy$" /etc/profile ; then echo http_proxy=$http_proxy >> /etc/profile ; fi
+  if ! grep -q -i "^https_proxy=$http_proxy$" /etc/profile ; then echo https_proxy=$https_proxy >> /etc/profile ; fi
+  if ! grep -q -i "^http_proxy=$http_proxy$" /etc/wgetrc ; then echo http_proxy=$http_proxy >> /etc/wgetrc ; fi
+fi 
 source /etc/profile
 #exit
 
